82659bf6c2be131ebe3da50922e8702283b5e5e6
Nivedha Rajendran <nivedha.rajen@gmail.com>
2024-10-07 14:23:15 +0200

    Struct Singletons

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -6,6 +6,7 @@ struct Restaurant {
     
 }
 
+//Singletons are used where we access one source of data or one value, one shared instance will be there
 //Singleton classes
 final class RestaurantManager {
     @MainActor static let shared = RestaurantManager()
@@ -40,3 +41,20 @@ RestaurantManager.shared.getData { _ in
 RestaurantManager.shared.getData { _ in
     print("Received items")
 }
+
+
+//Struct Singleton
+struct structSingleton {
+    var isLoading = false
+    
+    @MainActor static var shared = structSingleton()
+}
+
+
+var localCopy = structSingleton.shared
+localCopy.isLoading = true //localCopy will only be affected as struct is a value type
+
+print(structSingleton.shared.isLoading)// the value here will not be changed here, as only the copy of sharedinstance is changed and not the actual instance
+
+structSingleton.shared.isLoading = true //changing sharedinstance directly
+print(structSingleton.shared.isLoading)
